65658adf8da53f4f6684832f8b06d460d26cc070
Andrey Maslov <maslov.kng@gmail.com>
2021-03-13 22:30:00 +0200

    add prime and odd sort

--- homework_01/main.py
+++ homework_01/main.py
@@ -12,7 +12,7 @@ def power_numbers(*args):
     power_list = []
     power = 2
     for number in args:
-            power_list.append(number ** power)
+        power_list.append(number ** power)
     return power_list
 
 
@@ -22,12 +22,39 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def filter_numbers(nums_list, list_type):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
     """
+    if list_type == 'even':
+        event_list = []
+        for nums in nums_list:
+            if int(nums) % 2 == 0:
+                event_list.append(nums)
+        return event_list
 
+    if list_type == 'odd':
+        odd_list = []
+        for nums in nums_list:
+            if int(nums) % 2 != 0:
+                odd_list.append(nums)
+        return odd_list
 
-print('Power_numbers ' + str(power_numbers(1, 2, 3, 4, 5, 6, 7)))
+    if list_type == 'prime':
+        prime_list = []
+        for nums in nums_list:
+            if nums > 1:
+                count = 0
+                divisor = 2
+                while divisor < nums:
+                    if nums % divisor == 0:
+                        count += 1
+                    divisor += 1
+                if count == 0:
+                    prime_list.append(nums)
+        return prime_list
+
+# Example
+#print(filter_numbers([1, 2, 3, 4, 10, 24], PRIME))
